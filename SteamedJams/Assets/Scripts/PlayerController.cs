893ec155b17bdc2ad6bc359ed7bec09f8115e84c
JohnPlantGit <41980810+JohnPlantGit@users.noreply.github.com>
2019-05-22 16:37:18 +1000

    -Added Spot dodge to the player -Split the player's update into states -Stopped the player from attacking while dashing -Added Better attack and dash when in the window of opportunity

--- SteamedJams/Assets/Scripts/PlayerController.cs
+++ SteamedJams/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public enum Team
     Two
 };
 
+enum PlayerState
+{
+    Base,
+    Dashing,
+    Dodging
+};
+
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
@@ -24,9 +31,16 @@ public class PlayerController : MonoBehaviour
 
     [Header("Dash")]
     public float m_dashLength;
-    public float m_dashSpeed;
+    public float m_defaultDashSpeed;
     public HurtBox m_dashHurtBox;
     public int m_dashDamage;
+
+    [Header("Dodge")]
+    public float m_dodgeLength;
+
+    PlayerState m_currentState = PlayerState.Base;
+
+    AudioManager m_audioManager;
 
     Animator m_animator;
     bool m_canAttack = true;
@@ -34,16 +48,21 @@ public class PlayerController : MonoBehaviour
     CharacterController m_characterController;
     Vector3 m_velocity;
 
-    bool m_dashing;
     float m_dashTimer;
     Vector3 m_dashDirection;
+    public float m_dashSpeed;
+
+    bool m_dodging = false;
+    float m_dodgeTimer = 0;
 
     //TEMP
     //public Vector3 m_gravity;
     //
-
+    [Header("Debug")]
     [SerializeField]
     int m_health;
+    public Renderer m_renderer;
+    Color m_defaultColour;
 
     public int Health
     {
@@ -54,6 +73,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_audioManager = AudioManager.GetInstance();
+        m_audioManager.SetBGM(null);
+
+        m_defaultColour = m_renderer.material.color;
+
         m_health = m_maxHealth;
         m_animator = GetComponent<Animator>();
         m_characterController = GetComponent<CharacterController>();
@@ -63,64 +87,121 @@ public class PlayerController : MonoBehaviour
 
         m_dashHurtBox.m_damage = m_dashDamage;
         m_dashHurtBox.m_team = m_team;
+
+        m_dashSpeed = m_defaultDashSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!m_dashing)
+        switch (m_currentState)
         {
-            Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-            if (movement.sqrMagnitude != 0)
-            {
-                transform.forward = movement;
-            }
-
-            m_velocity += movement * m_speed * Time.deltaTime;
-            //m_velocity += m_gravity * Time.deltaTime;
-
-            m_characterController.Move(m_velocity * Time.deltaTime);
-
-            m_velocity -= m_velocity * m_drag * Time.deltaTime;      
-            
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_dashing = true;
-                m_dashHurtBox.enabled = true;
-                m_dashTimer = 0;
-
-                m_dashDirection = transform.forward;
-            }
-
-            if (Input.GetMouseButtonDown(0) && m_canAttack)
-            {
-                AttackStart();
-            }
-        }
-        else
-        {
-            Dashing();
+            case PlayerState.Base:
+                BaseState();
+                break;
+            case PlayerState.Dashing:
+                DashingState();
+                break;
+            case PlayerState.Dodging:
+                DodgingState();
+                break; 
         }
     }
 
-    void Dashing()
+    #region States
+    void BaseState()
     {
-        m_dashTimer += Time.deltaTime;
-
-        if (m_dashTimer < m_dashLength)
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (movement.sqrMagnitude != 0)
         {
-            m_characterController.Move(m_dashDirection * m_dashSpeed * Time.deltaTime);         
+            transform.forward = movement;
+        }
+
+        m_velocity += movement * m_speed * Time.deltaTime;
+        //m_velocity += m_gravity * Time.deltaTime;
+
+        m_characterController.Move(m_velocity * Time.deltaTime);
+
+        m_velocity -= m_velocity * m_drag * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0) && m_canAttack)
+        {
+            AttackStart();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!m_canAttack && !m_swordHurtBox.enabled)
+            {
+                AnimatorStateInfo currentAnim = m_animator.GetCurrentAnimatorStateInfo(0);
+                m_animator.Play("ReverseAttack", 0, 1 - currentAnim.normalizedTime);
+                m_canAttack = true;
+            }
+
+            m_dashHurtBox.enabled = true;
+            m_dashDirection = transform.forward;
+            if (!m_audioManager.IsInWindowOfOpportunity())
+            {
+                m_dashTimer = m_dashLength;
+                m_dashSpeed = m_defaultDashSpeed;
+            }
+            else
+            {
+                m_dashTimer = m_dashLength / 2;
+                m_dashSpeed = m_defaultDashSpeed * 2;
+            }
+
+            m_currentState = PlayerState.Dashing;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!m_canAttack && !m_swordHurtBox.enabled)
+            {
+                AnimatorStateInfo currentAnim = m_animator.GetCurrentAnimatorStateInfo(0);
+                m_animator.Play("ReverseAttack", 0, 1 - currentAnim.normalizedTime);
+                m_canAttack = true;
+            }
+
+            m_dodging = true;
+            m_dodgeTimer = m_dodgeLength;
+
+            m_renderer.material.color = new Color(0, 0, 0);
+
+            m_currentState = PlayerState.Dodging;
+        }   
+    }
+
+    void DashingState()
+    {
+        m_dashTimer -= Time.deltaTime;
+
+        if (m_dashTimer > 0)
+        {
+            m_characterController.Move(m_dashDirection * m_dashSpeed * Time.deltaTime);
         }
         else
         {
-            m_dashing = false;
             m_dashHurtBox.enabled = false;
+            m_currentState = PlayerState.Base;
         }
     }
+
+    void DodgingState()
+    {
+        m_dodgeTimer -= Time.deltaTime;
+        if (m_dodgeTimer <= 0)
+        {
+            m_dodging = false;
+            m_renderer.material.color = m_defaultColour;
+            m_currentState = PlayerState.Base;
+        }
+    }
+    #endregion
 
     public void TakeDamage(int damage)
     {
-        m_health -= damage;
+        if (!m_dodging)
+            m_health -= damage;
     }
 
     public void EnableSwordHurtBox()
@@ -136,8 +217,7 @@ public class PlayerController : MonoBehaviour
     void AttackStart()
     {
         m_canAttack = false;
-        bool inWindow = false; // TEMP REPLACE WITH BEAT SYSTEM
-        if (inWindow)
+        if (m_audioManager.IsInWindowOfOpportunity())
         {
             m_swordHurtBox.m_damage = m_swordDamage * 2;
             m_swordHurtBox.transform.localScale = new Vector3(0.2f, 0.2f, 2);
