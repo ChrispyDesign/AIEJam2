bd77b38aa4160e3d9df62076d8c51b422979195a
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2019-05-22 16:51:59 +1000

    Implemented all of the sound tracks.

---
+++ SteamedJams/Assets/Scripts/AudioTrack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioTrack", menuName = "Audio Track")]
+public class AudioTrack : ScriptableObject
+{
+    public float m_BPM;
+    public AudioClip m_AudioTrack;
+}
