df9cf3b252e1fdad911c3f3c2772d49d1dff2bba
JohnPlantGit <41980810+JohnPlantGit@users.noreply.github.com>
2019-05-23 16:28:54 +1000

    -Added script that plays a random animation

---
+++ SteamedJams/Assets/Scripts/PlayRandomAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayRandomAnimation : MonoBehaviour
+{
+    public int m_animationCount;
+
+    Animator m_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_animator.SetInteger("Random", Random.Range(0, m_animationCount));
+    }  
+}
