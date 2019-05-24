d02c15a3fba74685468c93831dcb4ac318b453f8
Nathan Burt <50722669+Burt07@users.noreply.github.com>
2019-05-24 16:31:39 +1000

    Health Bar

---
+++ SteamedJams/Assets/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider m_p1Scroll;
+    public Slider m_p2Scroll;
+
+    PlayerManager m_playerManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playerManager = PlayerManager.Instance;
+        m_p1Scroll.maxValue = m_playerManager.m_players[0].m_maxHealth;
+        m_p2Scroll.maxValue = m_playerManager.m_players[1].m_maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_p1Scroll.value = m_playerManager.m_players[0].Health;
+        m_p2Scroll.value = m_playerManager.m_players[1].Health;
+    }
+}
